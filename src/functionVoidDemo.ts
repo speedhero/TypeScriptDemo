1e33f5271871d5654305137bda41e161402c0252
speedhero <speedhero@126.com>
2017-10-11 19:05:04 +0800

    TypeScript练习

---
+++ src/functionVoidDemo.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by asiainfo on 2017/10/11.
+ */
+function hello():void{
+    console.log("Hello World!");
+}
+
+hello();
---
+++ src/null&undefined.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by asiainfo on 2017/10/11.
+ */
+let x:number;
+let y:number | undefined;
+let z:number | null | undefined;
+
+x=1;
+y=1;
+z=1;
+
+x=undefined;
+y=undefined;
+z=undefined;
+
+x=null;
+y=null;
+z=null;
